29b05e9ff8aae23e104dcf0f6cebcc8c0d012e14
teftoo <cbbe.estefani.torrico.ar@unifranz.edu.bo>
2026-06-26 12:34:35 -0400

    Ejercicio 20: 'Título: sunt aut facere repellat provident occaecati excepturi optio reprehenderit'

--- python-desarrollo/ejercicio20.py
+++ python-desarrollo/ejercicio20.py
@@ -1,15 +1,12 @@
 import requests
 
-# TODO: Completa la función para que:
-# 1. Haga una petición GET a:
-#    https://jsonplaceholder.typicode.com/posts/1
-# 2. Si el status code es 200, imprima el título con el formato:
-#    "Título: <titulo del post>"
-# 3. Si ocurre cualquier excepción, imprima:
-#    "Error al obtener el post"
-
 def obtener_post():
-    # TODO: tu código aquí
-    pass
+    try:
+        response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
+        if response.status_code == 200:
+            data = response.json()
+            print("Título: " + data["title"])
+    except Exception:
+        print("Error al obtener el post")
 
 obtener_post()
