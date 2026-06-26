df5b923341ede11981469c597426e678c4f144a7
teftoo <cbbe.estefani.torrico.ar@unifranz.edu.bo>
2026-06-26 12:10:29 -0400

    Ejercicios completados

--- web-desarrollo/ejercicio5.js
+++ web-desarrollo/ejercicio5.js
@@ -1,15 +1,7 @@
-// API pública: https://jsonplaceholder.typicode.com/users/1
-// Responde con un objeto que incluye, entre otros campos: { id: 1, name: "Leanne Graham", ... }
-
 async function obtenerUsuario() {
-  // TODO: Usa fetch para obtener los datos del usuario con id 1
-  const response = await /* tu código aquí */;
-
-  // TODO: Convierte la respuesta a JSON
-  const data = await /* tu código aquí */;
-
-  // TODO: Imprime en consola: "Usuario: " seguido del nombre del usuario
-  /* tu código aquí */
+  const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
+  const data = await response.json();
+  console.log("Usuario: " + data.name);
 }
 
 obtenerUsuario();
